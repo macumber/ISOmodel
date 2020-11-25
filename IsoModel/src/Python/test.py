ef498d51207fde4a18eff2c462fc049e6e63a10c
XXX <dan.macumber@gmail.com>
2020-11-24 21:24:58 -0700

    Run HourlyModel in test.py

--- IsoModel/src/Python/test.py
+++ IsoModel/src/Python/test.py
@@ -8,3 +8,12 @@ for result in results:
   print(result.getEndUse(isomodel.ElecCool))
 
 print(isomodel.totalEnergyUse(results))
+
+user_model = isomodel.UserModel()
+user_model.load("./test_data/defaults_test_building.ism", "./test_data/defaults_test_defaults.ism")
+hourly_model = user_model.toHourlyModel()
+results = hourly_model.simulate()
+for result in results:
+  print(result.getEndUse(isomodel.ElecCool))
+
+print(isomodel.totalEnergyUse(results))
